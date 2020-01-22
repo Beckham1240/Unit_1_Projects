b3ce42ad3a6cc3100ee8cf37d2752cee5ed568b7
Beckham <ble335west-mec.org>
2020-01-22 13:58:34 -0700

    Finished project 4

--- case4/ph_clock.js
+++ case4/ph_clock.js
@@ -1,3 +1,4 @@
+"use strict";
 /*
    New Perspectives on HTML5 and CSS3, 7th Edition
    Tutorial 9
@@ -22,6 +23,27 @@
 /* The checkTimer() function tests whether there is any time left to make the
    ticket order. If the time left is 0, the stopClock() function is run;
    otherwise nothing happens and the program continues to run. */
+var minsLeft = 30;
+var secsLeft = 15;
+var timeLeft = minsLeft * 60 + secsLeft;
+var clockID = setInterval("countDown()", 1000);
+
+//calculates the minutes and seconds and inserts them in the minutes and seconds ID and deduces the time by 1 
+function countDown(){
+   minsLeft = Math.floor(timeLeft / 60);
+   secsLeft = timeLeft - 60 * minsLeft;
+   var minsString = addLeadingZero(minsLeft);
+   var secsString = addLeadingZero(secsLeft);
+   document.getElementById("minutes").innerHTML = minsString;
+   document.getElementById("seconds").innerHTML = secsString;
+   checkTimer();
+   timeLeft--;
+}
+//stops the clock and presents the Order Expired message when timer runs out
+function stopClock(){
+   document.getElementById("TimeHead").insertAdjacentHTML('beforeEnd', "<br />(Order Expired)");
+   clockID = clearInterval(clockID);
+}
    
 function checkTimer() {
    if (timeLeft === 0) stopClock();
